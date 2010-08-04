c0ba2ddb749d4d0c3aa1827a305f601d26da36b1
niels.a.d <niels.a.d@a39907e2-f054-abe9-7f15-56c359600eb2>
2010-08-04 22:23:17 +0000

    Made it compile again in PHP5.
    git-svn-id: http://ganon.googlecode.com/svn/trunk@40 a39907e2-f054-abe9-7f15-56c359600eb2

--- gan_node_html.php
+++ gan_node_html.php
@@ -886,7 +886,7 @@ class HTML_Node {
 			foreach(array_keys($this->children) as $k) {
 				if (!$this->children[$k]->isTextOrComment()) {
 					if (++$count === $child) {
-						return $this->children[$k]
+						return $this->children[$k];
 					}
 					$last = $this->children[$k];
 				}
